679c26256de1d049ce7fe3620594ed275e0f6d34
Anaïs Thillaye <athillaye@operametrix.fr>
2022-06-23 16:57:28 +0200

    Add OPM logo display at startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
 #include <M5Core2.h>
+#include "opm.h"
 
-void setup() {
+void setup()
+{
   M5.begin();
+
+  M5.Lcd.drawJpg(opm, sizeof(opm), 75, 30);
+  delay(500);
+  M5.Lcd.clear();
+  
   M5.Lcd.setCursor(90, 110);
   M5.Lcd.setTextSize(2);
   M5.Lcd.print("Hello World !");
+  
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
