756869f58de55230f0d04092cfd39161ea757892
Anaïs Thillaye <athillaye@operametrix.fr>
2022-08-23 12:29:25 +0200

    Add Initial setup with LoraWAN message sending

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <Arduino.h>
 #include <M5Core2.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
 #include "opm.h"
 #include "plus.h"
 #include "minus.h"
@@ -37,43 +39,223 @@ unsigned long ts;
 unsigned long last_update;
 byte raw[30];
 
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8]={ 0};
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8]={ 0x3d ,0x1a, 0x14, 0x2f, 0x40, 0xd5, 0xa4, 0x7c };
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+static const u1_t PROGMEM APPKEY[16] = { 0x16, 0xAE, 0xDC, 0x29, 0x80, 0x22, 0xE9, 0x30, 0x33, 0x8D, 0x24, 0xFE, 0x82, 0x0E, 0x42, 0x3A };
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+static uint8_t mydata[] = "Hello, world!";
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+// Pin mapping
+// https://docs.m5stack.com/en/module/lora868
+const lmic_pinmap lmic_pins = {
+  .nss = 33,                       
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = 26,                       
+  .dio = {36, 35, LMIC_UNUSED_PIN}, 
+};
+
+void printHex2(unsigned v) {
+  v &= 0xff;
+  if (v < 16)
+    Serial.print('0');
+  Serial.print(v, HEX);
+}
+
+void do_send(osjob_t* j){
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+      Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+      // Prepare upstream data transmission at the next possible time.
+      LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+      Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent (ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch(ev) {
+      case EV_SCAN_TIMEOUT:
+          Serial.println(F("EV_SCAN_TIMEOUT"));
+          break;
+      case EV_BEACON_FOUND:
+          Serial.println(F("EV_BEACON_FOUND"));
+          break;
+      case EV_BEACON_MISSED:
+          Serial.println(F("EV_BEACON_MISSED"));
+          break;
+      case EV_BEACON_TRACKED:
+          Serial.println(F("EV_BEACON_TRACKED"));
+          break;
+      case EV_JOINING:
+          Serial.println(F("EV_JOINING"));
+          break;
+      case EV_JOINED:
+          Serial.println(F("EV_JOINED"));
+          {
+            u4_t netid = 0;
+            devaddr_t devaddr = 0;
+            u1_t nwkKey[16];
+            u1_t artKey[16];
+            LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+            Serial.print("netid: ");
+            Serial.println(netid, DEC);
+            Serial.print("devaddr: ");
+            Serial.println(devaddr, HEX);
+            Serial.print("AppSKey: ");
+            for (size_t i=0; i<sizeof(artKey); ++i) {
+              if (i != 0)
+                Serial.print("-");
+              printHex2(artKey[i]);
+            }
+            Serial.println("");
+            Serial.print("NwkSKey: ");
+            for (size_t i=0; i<sizeof(nwkKey); ++i) {
+                    if (i != 0)
+                            Serial.print("-");
+                    printHex2(nwkKey[i]);
+            }
+            Serial.println();
+          }
+          // Disable link check validation (automatically enabled
+          // during join, but because slow data rates change max TX
+   // size, we don't use it in this example.
+          LMIC_setLinkCheckMode(0);
+          break;
+      /*
+      || This event is defined but not used in the code. No
+      || point in wasting codespace on it.
+      ||
+      || case EV_RFU1:
+      ||     Serial.println(F("EV_RFU1"));
+      ||     break;
+      */
+      case EV_JOIN_FAILED:
+          Serial.println(F("EV_JOIN_FAILED"));
+          break;
+      case EV_REJOIN_FAILED:
+          Serial.println(F("EV_REJOIN_FAILED"));
+          break;
+      case EV_TXCOMPLETE:
+          Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+          if (LMIC.txrxFlags & TXRX_ACK)
+            Serial.println(F("Received ack"));
+          if (LMIC.dataLen) {
+            Serial.print(F("Received "));
+            Serial.print(LMIC.dataLen);
+            Serial.println(F(" bytes of payload"));
+          }
+          // Schedule next transmission
+          os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+          break;
+      case EV_LOST_TSYNC:
+          Serial.println(F("EV_LOST_TSYNC"));
+          break;
+      case EV_RESET:
+          Serial.println(F("EV_RESET"));
+          break;
+      case EV_RXCOMPLETE:
+          // data received in ping slot
+          Serial.println(F("EV_RXCOMPLETE"));
+          break;
+      case EV_LINK_DEAD:
+          Serial.println(F("EV_LINK_DEAD"));
+          break;
+      case EV_LINK_ALIVE:
+          Serial.println(F("EV_LINK_ALIVE"));
+          break;
+      /*
+      || This event is defined but not used in the code. No
+      || point in wasting codespace on it.
+      ||
+      || case EV_SCAN_FOUND:
+      ||    Serial.println(F("EV_SCAN_FOUND"));
+      ||    break;
+      */
+      case EV_TXSTART:
+          Serial.println(F("EV_TXSTART"));
+          break;
+      case EV_TXCANCELED:
+          Serial.println(F("EV_TXCANCELED"));
+          break;
+      case EV_RXSTART:
+          /* do not print anything -- it wrecks timing */
+          break;
+      case EV_JOIN_TXCOMPLETE:
+          Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+          break;
+       default:
+          Serial.print(F("Unknown event: "));
+          Serial.println((unsigned) ev);
+          break;
+  }
+}
+
 void vibratorSetup() {
-    ledcSetup(VIBRATOR_PWM_CHANNEL, VIBRATOR_PWM_FREQ, VIBRATOR_PWM_RESOLUTION);
-    ledcAttachPin(VIBRATOR_PIN, VIBRATOR_PWM_CHANNEL);
+  ledcSetup(VIBRATOR_PWM_CHANNEL, VIBRATOR_PWM_FREQ, VIBRATOR_PWM_RESOLUTION);
+  ledcAttachPin(VIBRATOR_PIN, VIBRATOR_PWM_CHANNEL);
 }
 
 void vibratorSet(uint32_t duty) {
-    ledcWrite(VIBRATOR_PWM_CHANNEL, duty);
+  ledcWrite(VIBRATOR_PWM_CHANNEL, duty);
 }
 
 void encodestruct(struct sensorData_t data,byte bytes[2]) {
-    signed short temp = data.temperature *100;
-    bytes[0] = (temp>>8) & 0xFF;
-    bytes[1] = temp  & 0xFF;
-    signed short hum = data.humidity *100;
-    bytes[2] = (hum>>8) & 0xFF;
-    bytes[3] = hum  & 0xFF;
-    signed pres = data.pressure ;
-    Serial.println(pres);
-    bytes[4] = (pres>>16) & 0xFF;
-    bytes[5] = (pres>>8) & 0xFF;
-    bytes[6] = pres  & 0xFF;
-    unsigned long ts = data.timestamp;
-    bytes[7] = (ts>>8) & 0xFF;
-    bytes[8] = ts  & 0xFF;
+  signed short temp = data.temperature *100;
+  bytes[0] = (temp>>8) & 0xFF;
+  bytes[1] = temp  & 0xFF;
+  signed short hum = data.humidity *100;
+  bytes[2] = (hum>>8) & 0xFF;
+  bytes[3] = hum  & 0xFF;
+  signed pres = data.pressure ;
+  Serial.println(pres);
+  bytes[4] = (pres>>16) & 0xFF;
+  bytes[5] = (pres>>8) & 0xFF;
+  bytes[6] = pres  & 0xFF;
+  unsigned long ts = data.timestamp;
+  bytes[7] = (ts>>8) & 0xFF;
+  bytes[8] = ts  & 0xFF;
 };
 
-void setup()
-{
+void setup() {
   M5.begin(true, false, true, false);
-  
+
+  // LMIC init
+  os_init();
+
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+
   #ifdef EXTERNAL_BUTTON
   pinMode(32, INPUT);
   #endif
 
   #ifdef VIBRATOR
-    vibratorSetup();
-    vibratorSet(0);
+  vibratorSetup();
+  vibratorSet(0);
   #endif
 
   Wire.begin(14,13);
@@ -86,11 +268,9 @@ void setup()
  /* M5.Lcd.setCursor(90, 110);
   M5.Lcd.setTextSize(2);
   M5.Lcd.print("Hello World !"); */
-
 }
 
-void loop()
-{
+void loop() {
   ts = millis();
   M5.update();
 
@@ -185,4 +365,5 @@ void loop()
     data.timestamp = last_update;
     encodestruct(data,raw);
   }
+os_runloop_once();
 }
